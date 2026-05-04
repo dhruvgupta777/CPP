f23af13b146efd9263eea025311d78f13d451693
DHRUV GUPTA <dhruvgupta0107@gmail.com>
2026-05-04 21:42:56 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a,b,c;
+    double average;
+
+    cout << " enter three number: ";
+    cin >> a >> b >> c;
+    average = (a + b + c) / 3.0;
+    cout << "average is: " << average << endl;
+}
